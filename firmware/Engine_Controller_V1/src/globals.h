61b9084412a2afb90dffc5f7a899c91f581462b7
nsternie <nsternie@umich.edu>
2017-11-04 14:01:27 -0400

    Moves all the globals and calibrations to their own files

---
+++ firmware/Engine_Controller_V1/src/globals.h
@@ -0,0 +1,166 @@
+#include <stdint.h>
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define HEADER_1 0
+#define HEADER_2 0
+#define HEADER_3 0
+#define HEADER_4 0
+#define HEADER_5 0
+#define HEADER_6 0
+#define HEADER_7 0
+#define HEADER_8 0
+
+// END TELEM DEFINITIONS //////////////////////////////////
+
+// Device definitions  ////////////////////////////////////
+#define vlv0 20
+#define vlv1 21
+#define vlv2 22
+#define vlv3 23
+#define vlv4 24
+#define vlv5 25
+#define vlv6 26
+#define vlv7 27
+#define vlv8 28
+#define vlv9 29
+#define vlv10 30
+#define vlv11 31
+#define vlv12 32
+#define vlv13 33
+#define vlv14 34
+#define vlv15 35
+
+#define led0 39
+
+#define mtr0 40
+#define mtr1 41
+#define mtr2 42
+#define mtr3 43
+
+#define adc0 50
+#define adc1 51
+#define adc2 52
+#define adc3 53
+#define adc4 54
+
+#define sram 60
+#define flash 61
+
+#define tc0 70
+#define tc1 71
+#define tc2 72
+#define tc3 73
+
+#define spirit 80
+
+#define rs422_com 	huart1
+#define xbee_com 	huart4
+#define spirit_com 	huart5
+
+#define rs422 	0
+#define xbee	1
+//#define spirit	2
+
+#define MANUAL 				1
+#define ARMED 				2
+#define IGNITION			3
+#define FIRING	 			4
+#define FULL_DURATION	 	5
+
+#define MAX_COMMAND_ARGS 	7
+#define MAX_COMMAND_LENGTH 	32
+#define COMMAND_HISTORY 5
+
+#define UART_BUFFER_SIZE 	1024
+#define COMMAND_BUFFER_LENGTH 64
+#define COMMAND_SOURCE 0
+
+
+#define none			0
+#define full_packet 	1
+#define small_packet 	2
+#define full_pretty		3
+#define small_pretty	4
+#define gui_v1			5
+#define gui_byte_packet	6
+
+
+
+
+extern uint8_t STATE;
+struct buffer{
+	uint8_t *start;
+	uint8_t *end;
+	uint16_t length;
+	uint16_t filled;
+	uint8_t *head;
+	uint8_t *tail;
+	uint8_t id;
+	uint8_t new_data;
+};
+extern char* states[10][15];
+extern volatile uint8_t read_adc_now;
+extern volatile uint8_t send_rs422_now;
+extern volatile uint8_t send_xbee_now;
+extern volatile uint8_t update_motors_now;
+extern uint16_t adc_data[5][16];		// 5 ADCs, 16 channels each.
+extern uint16_t valve_states;
+extern uint32_t IGNITION_DURATION;
+extern uint32_t FIRING_DURATION;
+extern uint32_t POST_IGNITE_DELAY;
+extern float motor_position[4];
+extern float motor_last_position[4];
+extern float motor_accumulated_error[4];
+extern int16_t motor_pwm[4];
+extern uint8_t motor_active[4];
+extern uint8_t LOGGING_ACTIVE;
+extern uint8_t spirit_data_buf[UART_BUFFER_SIZE];
+extern uint8_t rs422_data_buf[UART_BUFFER_SIZE];
+extern uint8_t xbee_data_buf[UART_BUFFER_SIZE];
+extern struct buffer spirit_buf;
+extern struct buffer rs422_buf;
+extern struct buffer xbee_buf;
+extern uint8_t spirit_in;							// Temp single byte buffer for rx
+extern uint8_t rs422_in;							// Temp single byte buffer for rx
+extern uint8_t xbee_in;							// Temp single byte buffer for rx
+extern uint8_t command_buffer[COMMAND_HISTORY][COMMAND_BUFFER_LENGTH];
+extern uint8_t command_index;
+extern uint8_t telem_unstuffed[254];
+extern uint8_t telem_stuffed[256];
+extern uint8_t unstuffed_packet_length;
+extern float evlv[16];
+extern float ivlv[16];
+extern float ebatt;
+extern float ibus;
+extern float e5v;
+extern float e3v;
+extern float tbrd, tvlv, tmtr;
+extern float pressure[16];
+extern float load[6];
+extern float thrust_load;
+extern float thermocouple[4];
+extern uint8_t temp;
+extern uint32_t count1;
+extern uint32_t count2;
+extern uint32_t count3;
+extern uint32_t start_time;
+extern uint32_t end_time;
+extern uint32_t active_time;
+extern uint32_t idle_time;
+extern float utilization;
+extern uint32_t state_timer;
+extern uint32_t 	motor_cycle_time[2],
+			main_cycle_time[2],
+			adc_cycle_time[2],
+			telemetry_cycle_time[2],
+			telem_utilization_time[2];
+extern uint16_t samplerate; // Hz
+extern uint8_t telemetry_format[3];
+extern uint16_t telemetry_rate[3];
+extern uint8_t device_alias[100][10];
+//
+
+
+#endif
