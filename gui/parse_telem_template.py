4d41c0cea8e228d8a0fd8e98ece65c433240d378
nsternie <nsternie@umich.edu>
2017-11-03 20:30:04 -0400

    Adds some error checking and telem validation

--- gui/parse_telem_template.py
+++ gui/parse_telem_template.py
@@ -1,6 +1,7 @@
 filename = input("Template file: ")
 
 template_file = open(filename)
+print("Reading "+filename+"...")
 
 format_string = ""
 argument_string = ""
@@ -9,6 +10,8 @@ globals_string = "\t## GLOBALS ##\n"
 device_list = []
 col = {}
 packet_byte_length = 1; # Always will have at least \n
+
+pack_telem_h_string = ""
 
 type_byte_lengths = {
 	"char"		:	1,
@@ -21,7 +24,30 @@ type_byte_lengths = {
 	"uint64_t"	:	8,
 	"int64_t"	:	8
 }
-		
+
+type_range_positive = {
+	"char"		:	255,
+	"uint8_t"	:	(2**8)-1,
+	"int8_t"	:	((2**8)/2)-1,
+	"uint16_t"	:	(2**16)-1,
+	"int16_t"	:	((2**16)/2)-1,
+	"uint32_t"	:	(2**32)-1,
+	"int32_t"	:	((2**32)/2)-1,
+	"uint64_t"	:	(2**64)-1,
+	"int64_t"	:	((2**64)/2)-1
+}
+
+type_range_negative = {
+	"char"		:	0,
+	"uint8_t"	:	0,
+	"int8_t"	:	-((2**8)/2),
+	"uint16_t"	:	0,
+	"int16_t"	:	-((2**16)/2),
+	"uint32_t"	:	0,
+	"int32_t"	:	-((2**32)/2),
+	"uint64_t"	:	0,
+	"int64_t"	:	-((2**64)/2)
+}
 
 n = 0
 for line in template_file:
@@ -47,8 +73,30 @@ for line in template_file:
 		xmit_scale  = split_string[col['xmit_scale']]
 		device  = split_string[col['device']]
 		python_variable_override  = split_string[col['python_variable_override']]
-		python_type  = split_string[col['python_globals']]
-		python_globals  = split_string[col['firmware_variable']]
+		python_type  = split_string[col['python_type']]
+		python_globals  = split_string[col['python_globals']]
+
+		try:
+			assert type_cast in type_byte_lengths.keys();
+		except:
+			print("Invalid type cast. Valid Types are:\n"+str(type_byte_lengths.keys()))
+
+		## Check xmit limits
+		try:
+			if(min_val):
+				assert (float(min_val)*float(xmit_scale) >= type_range_negative[type_cast])
+		except:
+			print("Invalid type cast for given range on item "+name	)
+			print("Min val: "+str(float(min_val)*float(xmit_scale)))
+			print("Type limit: "+str(type_range_negative[type_cast]))
+		try:
+			if(max_val):
+				assert (float(max_val)*float(xmit_scale) <= type_range_positive[type_cast])
+		except:
+			print("Invalid type cast for given range on item "+name	)
+			print("Max val: "+str(float(max_val)*float(xmit_scale)))
+			print("Type limit: "+str(type_range_positive[type_cast]))
+		## End check xmit limits
 
 		byte_length = type_byte_lengths[type_cast]
 		packet_byte_length += byte_length
@@ -56,15 +104,17 @@ for line in template_file:
 		format_string = format_string + printf_format.rstrip('\n')+','
 		argument_string = argument_string + (","+firmware_variable)
 
+		# Parse Globals
 		if(python_globals):
 			globals_string = globals_string + "\tglobal "+python_globals+'\n'
 
 		if(python_variable_override):
-			python_string = python_string + "\t"+python_variable_override+" = "+python_type+"(split_line["+str(n-1)+"])\n"
+			python_string += "\t"+python_variable_override+" = "+python_type+"((float(split_line["+str(n-1)+"]))/"+xmit_scale+")\n"
 		else:
-			python_string = python_string + "\t"+firmware_variable+" = "+python_type+"(split_line["+str(n-1)+"])\n"
+			python_string += "\t"+firmware_variable+" = "+python_type+"((float(split_line["+str(n-1)+"]))/"+xmit_scale+")\n"
 
 		device_list.append(split_string[5])
+		pack_telem_h_string += "#define\tTELEM_ITEM_"+str(n-1)+"\t"+str(firmware_variable)+"\n"
 
 	n += 1;
 
@@ -86,11 +136,14 @@ for line in template_file:
 csv_header = ""#csv_header + "\"\n\n"
 parsed_printf_file = open((filename+"_sprintf-call_.c"),"w+")
 parsed_python_file = open(("hotfire_packet.py"),"w+")
+pack_telem_h = open("pack_telem.h", "w+")
 
 parsed_printf_file.write("snprintf(line, sizeof(line), \""+format_string+"\\r\\n\""+argument_string+");")
-parsed_python_file.write("write_csv_header = True\ndef parse_packet(split_line):\n"+globals_string+python_string)
+parsed_python_file.write("def parse_packet(split_line):\n"+globals_string+python_string)
+pack_telem_h.write(pack_telem_h_string)
 
-print("Packet statistics:")
+print("Template Sucessfully Parsed!")
+print(" --- Packet statistics --- ")
 print("Packet items: "+str(n))
-print("Packet length (bytes): "+str(packet_byte_length))
+print("Packet length (bytes): "+str(packet_byte_length-1))
 
