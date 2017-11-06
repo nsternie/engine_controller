fc37ac229ef82815d936512b71c3db784ccea44c
nsternie <nsternie@umich.edu>
2017-11-06 18:48:27 -0500

    gui runs

--- gui/hotfire_v0.py
+++ gui/hotfire_v0.py
@@ -4,6 +4,8 @@ import pyqtgraph as pg
 import serial
 import time
 import os
+from hotfire_packet import ECParse
+import struct
 
 
 # Gloabals
@@ -77,14 +79,15 @@ def parse_serial():
 
 	if(ser.is_open):
 		line = ser.readline()	
-		line = str(line, 'ascii')
-		try:
-			split_line = line.split(',')
-			parse_packet(split_line)
-			serial_log.write("%.3f," % time.clock())
-			serial_log.write(line.rstrip('\n'))
-		except:
-			pass
+		#line = str(line, 'ascii')
+		#try:
+			#split_line = line.split(',')
+		parse_packet(line)
+		#serial_log.write("%.3f," % time.clock())
+		#serial_log.write(line.rstrip('\n'))
+		# except:
+		# 	print("Error")
+		# 	pass
 		# print("Packet parsed")
 		# print("battery: "+str(ebatt)+" \t and %.2f" % time.clock())
 
@@ -158,7 +161,8 @@ load = [0,0,0,0]
 thrust_load = 0
 thermocouple = [0, 0, 0, 0]
 
-def parse_packet(split_line):
+def parse_packet(packet):
+		## GLOBALS ##
 	global valve_states
 	global pressure
 	global samplerate
@@ -180,45 +184,85 @@ def parse_packet(split_line):
 	global load
 	global thrust_load
 	global thermouple
-	valve_states = int(split_line[0])
-	pressure[0] = float(split_line[1])
-	pressure[1] = float(split_line[2])
-	pressure[2] = float(split_line[3])
-	pressure[3] = float(split_line[4])
-	pressure[4] = float(split_line[5])
-	pressure[5] = float(split_line[6])
-	pressure[6] = float(split_line[7])
-	pressure[7] = float(split_line[8])
-	samplerate = int(split_line[9])
-	motor_setpoint[0] = float(split_line[10])
-	motor_setpoint[1] = float(split_line[11])
-	main_cycle_time = int(split_line[12])
-	motor_cycle_time = int(split_line[13])
-	adc_cycle_time = int(split_line[14])
-	telemetry_cycle_time = int(split_line[15])
-	ebatt = float(split_line[16])
-	ibus = float(split_line[17])
-	telemetry_rate = int(split_line[18])
-	motor_control_gain[0] = float(split_line[19])
-	motor_control_gain[1] = float(split_line[20])
-	motor_control_gain[2] = float(split_line[21])
-	motor_position[0] = float(split_line[22])
-	motor_position[1] = float(split_line[23])
-	motor_pwm[0] = int(split_line[24])
-	motor_pwm[1] = int(split_line[25])
-	count1 = int(split_line[26])
-	count2 = int(split_line[27])
-	count3 = int(split_line[28])
-	STATE = int(split_line[29])
-	load[0] = float(split_line[30])
-	load[1] = float(split_line[31])
-	load[2] = float(split_line[32])
-	load[3] = float(split_line[33])
-	thrust_load = float(split_line[34])
-	thermocouple[0] = int(split_line[35])
-	thermocouple[1] = int(split_line[36])
-	thermocouple[2] = int(split_line[37])
-	thermocouple[3] = int(split_line[38])
+	byte_rep = packet[0:2]
+	valve_states = int((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[2:4]
+	pressure[0] = float((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[4:6]
+	pressure[1] = float((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[6:8]
+	pressure[2] = float((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[8:10]
+	pressure[3] = float((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[10:12]
+	pressure[4] = float((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[12:14]
+	pressure[5] = float((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[14:16]
+	pressure[6] = float((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[16:18]
+	pressure[7] = float((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[18:22]
+	samplerate = int((float(struct.unpack(">I", byte_rep)[0]))/1)
+	byte_rep = packet[22:26]
+	motor_setpoint[0] = float((float(struct.unpack(">i", byte_rep)[0]))/1000)
+	byte_rep = packet[26:30]
+	motor_setpoint[1] = float((float(struct.unpack(">i", byte_rep)[0]))/1000)
+	byte_rep = packet[30:32]
+	main_cycle_time = int((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[32:34]
+	motor_cycle_time = int((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[34:36]
+	adc_cycle_time = int((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[36:38]
+	telemetry_cycle_time = int((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[38:40]
+	ebatt = float((float(struct.unpack(">h", byte_rep)[0]))/1000)
+	byte_rep = packet[40:42]
+	ibus = float((float(struct.unpack(">h", byte_rep)[0]))/100)
+	byte_rep = packet[42:44]
+	telemetry_rate = int((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[44:46]
+	motor_control_gain[0] = float((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[46:48]
+	motor_control_gain[1] = float((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[48:50]
+	motor_control_gain[2] = float((float(struct.unpack(">H", byte_rep)[0]))/1)
+	byte_rep = packet[50:54]
+	motor_position[0] = float((float(struct.unpack(">i", byte_rep)[0]))/1000)
+	byte_rep = packet[54:58]
+	motor_position[1] = float((float(struct.unpack(">i", byte_rep)[0]))/1000)
+	byte_rep = packet[58:60]
+	motor_pwm[0] = int((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[60:62]
+	motor_pwm[1] = int((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[62:66]
+	count1 = int((float(struct.unpack(">I", byte_rep)[0]))/1)
+	byte_rep = packet[66:70]
+	count2 = int((float(struct.unpack(">I", byte_rep)[0]))/1)
+	byte_rep = packet[70:74]
+	count3 = int((float(struct.unpack(">I", byte_rep)[0]))/1)
+	byte_rep = packet[74:75]
+	STATE = int((float(struct.unpack(">B", byte_rep)[0]))/1)
+	byte_rep = packet[75:77]
+	load[0] = float((float(struct.unpack(">h", byte_rep)[0]))/10)
+	byte_rep = packet[77:79]
+	load[1] = float((float(struct.unpack(">h", byte_rep)[0]))/10)
+	byte_rep = packet[79:81]
+	load[2] = float((float(struct.unpack(">h", byte_rep)[0]))/10)
+	byte_rep = packet[81:83]
+	load[3] = float((float(struct.unpack(">h", byte_rep)[0]))/10)
+	byte_rep = packet[83:87]
+	thrust_load = float((float(struct.unpack(">i", byte_rep)[0]))/10)
+	byte_rep = packet[87:89]
+	thermocouple[0] = int((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[89:91]
+	thermocouple[1] = int((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[91:93]
+	thermocouple[2] = int((float(struct.unpack(">h", byte_rep)[0]))/1)
+	byte_rep = packet[93:95]
+	thermocouple[3] = int((float(struct.unpack(">h", byte_rep)[0]))/1)
+
 
 
 
