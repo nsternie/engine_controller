1655f740bd74542a5a63e961fa0befd6a6b1924e
Nick <nsternie@umich.edu>
2018-09-18 14:49:37 -0400

    Cleaned up noise for new development

--- gui/serial_pass.py
+++
@@ -1,4 +0,0 @@
-import serial
-ser = serial.Serial(port="COM12", baudrate=115200, timeout=0.5)
-while(1):
-	print(ser.readline())
--- gui/sort_data.sh
+++
@@ -1,9 +0,0 @@
-cd $1
-mkdir datalogs
-mkdir commandlogs
-mkdir seriallogs
-mkdir pythonlogs
-mv *_datalog* datalogs
-mv *_command_log* commandlogs
-mv *_serial_log* seriallogs
-mv *_python_log* pythonlogs
